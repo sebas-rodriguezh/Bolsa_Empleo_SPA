2efabe6311b252fbededf20d9d375fc502ae113c
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2026-05-05 12:08:56 -0600

    Repo listo para trabajarse

--- src/main/java/com/example/bolsadeempleo/presentation/administrador/Controller.java
+++ src/main/java/com/example/bolsadeempleo/presentation/administrador/Controller.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Map;
 
+// Acá van los principales cambios del Backend.
+
+
 @org.springframework.stereotype.Controller("administrador")
 public class Controller {
     @Autowired
